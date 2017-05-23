a803b45c374e884e99d702d89c0f1827ed1aa32c
Vilcho <vilcho@yahoo.com>
2017-05-24 01:43:29 +0300

    0

--- src/Lab_p03_PriceChangeAlert.java
+++ src/Lab_p03_PriceChangeAlert.java
@@ -1,11 +1,18 @@
+
+
 import java.text.DecimalFormat;
+
 import java.text.NumberFormat;
+
 import java.text.ParseException;
+
 import java.util.Scanner;
+
 
 /**
  * Created by Vilcho on 5/22/2017.
  */
+
 public class Lab_p03_PriceChangeAlert {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -20,51 +27,61 @@ public class Lab_p03_PriceChangeAlert {
 
         for (int i = 0; i < n-1; i++) {
             newPrice = Double.parseDouble(scanner.nextLine());
-            double changeInPrice = priceChange(temporaryPreviousPrice, newPrice);
-            changeAlert += changeSignificance(percentage(temporaryPreviousPrice, newPrice), significanceThreshold) + priceChangePercentage(temporaryPreviousPrice, newPrice);
+            double percentage = percentageChange(temporaryPreviousPrice, newPrice);
+            double percentageBy100 = percentage * 100.0;
+            boolean eitherTrueOrFalse = isEnoughDiff(significanceThreshold, percentage);
+            changeAlert += changeSignificance(percentage, significanceThreshold, newPrice, temporaryPreviousPrice, eitherTrueOrFalse) + calcDetails(temporaryPreviousPrice, newPrice, percentage);
+            if (newPrice - temporaryPreviousPrice != 0) {
+                changeAlert += String.format(" (%.2f%%)%n", percentageBy100);
+            }
+// else {
+//                changeAlert += System.lineSeparator();
+//            }
             temporaryPreviousPrice = newPrice;
         }
         System.out.println(changeAlert);
     }
 
-    private static String priceChangePercentage(double temporaryPreviousPrice, double newPrice) {
-        double percentageOfChangeInPrice = (newPrice - temporaryPreviousPrice) / temporaryPreviousPrice * 100;
+    static String calcDetails(double temporaryPreviousPrice, double newPrice, double percentage) {
+        String details = "";
         NumberFormat formatter = new DecimalFormat("###########.##");
-        String displayDetails = "";
-        if (percentageOfChangeInPrice != 0) {
-            displayDetails = formatter.format(temporaryPreviousPrice) + " to " + formatter.format(newPrice) + String.format(" (%.2f%%)%n", percentageOfChangeInPrice);
+        if (percentage != 0 && !String.valueOf(percentage).equals("NaN")) {
+            details = formatter.format(temporaryPreviousPrice) + " to " + formatter.format(newPrice);
         } else {
-            displayDetails = formatter.format(temporaryPreviousPrice) + System.lineSeparator();
+            details = formatter.format(temporaryPreviousPrice) + System.lineSeparator();
         }
-        return displayDetails;
-    }
-    static double percentage (double temporaryPreviousPrice, double newPrice){
-        double percentage = 0;
-        if (newPrice > temporaryPreviousPrice) {
-            percentage = (newPrice - temporaryPreviousPrice) / temporaryPreviousPrice * 100;
-        } else if (newPrice < temporaryPreviousPrice){
-            percentage = (temporaryPreviousPrice - newPrice) / newPrice * 100;
-        }
-        return percentage;
+        return details;
     }
 
-    private static String changeSignificance(double percentage, double significanceThreshold) {
+    static String changeSignificance(double percentage, double significanceThreshold, double newPrice, double temporaryPreviousPrice, boolean eitherTrueOfFalse) {
         String priceSignificanceChange = "";
-        significanceThreshold *= 100;
-        if (percentage == 0){
+        boolean eitherTrueOrFalse = isEnoughDiff(significanceThreshold, percentage);
+        if (percentage == 0 || String.valueOf(percentage).equals("NaN")) {
             priceSignificanceChange = "NO CHANGE: ";
-        } else if ((percentage < significanceThreshold && percentage > 0) || (percentage < significanceThreshold && (!((Math.abs(percentage) > significanceThreshold)))) ){
+        } else if (!eitherTrueOrFalse) {
             priceSignificanceChange = "MINOR CHANGE: ";
-        } else if (percentage >= significanceThreshold && percentage > 0){
+        } else if (eitherTrueOfFalse && (percentage > 0)) {
             priceSignificanceChange = "PRICE UP: ";
-        } else if (percentage >= significanceThreshold && percentage <0){
+        } else if (eitherTrueOfFalse && (percentage < 0)) {
             priceSignificanceChange = "PRICE DOWN: ";
         }
         return priceSignificanceChange;
+}
+
+
+    static boolean isEnoughDiff(double significanceThreshold, double percentage) {
+        boolean eitherTrueOrFalse = false;
+        if (Math.abs(significanceThreshold) <= Math.abs(percentage)) {
+            eitherTrueOrFalse = true;
+        } else {
+            eitherTrueOrFalse = false;
+        }
+        return eitherTrueOrFalse;
     }
 
-    static double priceChange (double temporaryPreviousPrice, double newPrice){
+    static double percentageChange(double temporaryPreviousPrice, double newPrice) {
         double changeInPrice = newPrice - temporaryPreviousPrice;
-        return changeInPrice;
+        double percentage = changeInPrice / temporaryPreviousPrice;
+        return percentage;
     }
 }
